e7036ab146ad371110e71fffd8b978f4a6674c2f
Essi Parent <9ivko1ozf@relay.firefox.com>
2024-10-04 15:34:44 -0400

    Bump version: 0.2.1 → 0.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'djalgo'
 copyright = '2024, Essi Parent'
 author = 'Essi Parent'
-release = "0.2.1"
+release = "0.2.2"
 
 # Extensions
 extensions = [
