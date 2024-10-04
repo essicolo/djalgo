97c7bd3f48aa4d570280a3756243740d176de79b
Essi Parent <9ivko1ozf@relay.firefox.com>
2024-10-04 11:01:22 -0400

    clean

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,5 @@
-# Configuration file for the Sphinx documentation builder.
-
 import os
 import sys
-
 sys.path.insert(0, os.path.abspath('..'))
 
 # Project information
@@ -22,55 +19,18 @@ extensions = [
 ]
 
 # General configuration
-source_dir = 'source'
 master_doc = 'index'
-source_suffix = ['.rst', '.md', '.ipynb']
-source_encoding = 'utf-8'
-source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+    '.ipynb': 'nbsphinx',
+}
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # HTML output configuration
 html_theme = 'furo'
-htmlhelp_basename = 'djalgoDoc'
 html_static_path = ['_static']
 templates_path = ['_templates']
-html_output_path = '_build/html'
-html_logo = '_static/logo.png'
 
-# Sidebar configuration
-html_sidebars = {
-    "**": [
-        "sidebar/scroll-start.html",
-        "sidebar/brand.html",
-        "sidebar/search.html",
-        "sidebar/navigation.html",
-        "sidebar/ethical-ads.html",
-        "sidebar/scroll-end.html",
-    ]
-}
-
-# Intersphinx mapping
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
-}
-
-# Napoleon settings
-napoleon_google_docstring = True
-napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
-napoleon_preprocess_types = False
-napoleon_type_aliases = None
-napoleon_attr_annotations = True
-
-# NBSphinx settings
+# Nbsphinx settings
 nbsphinx_execute = 'never'
--- docs/index.rst
+++ docs/index.rst
@@ -4,15 +4,16 @@ Welcome to Djalgo's documentation!
 Djalgo is a Python toolkit for music composition. Crafted for musicians and developers alike, Djalgo offers a versatile collection of tools for generating and manipulating musical notes and sequences. Without relying directly on MIDI files, it employs a MIDI-compatible format, enabling users to define each note through a trio of attributes: pitch (in MIDI notation), duration, and offset from the begining of the track. Whether you're scripting a symphony, experimenting with algorithms to create melodies, or exploring the mathematical beauty of music, Djalgo makes the art of digital composition accessible and engaging. The name Djalgo was chosen after musician `Django Reinhardt <https://en.wikipedia.org/wiki/Django_Reinhardt>`_, and a mix of *DJ* (disk jockey) and *algorithm*.
 
 .. toctree::
-   :maxdepth: 1
+   :maxdepth: 2
+   :caption: Contents:
 
-   01_getting-started
-   02_harmony
-   03_loops
-   04_minimalism
-   05_walks
-   06_fractals
-   07_genetic
-   08_ai
-   api
-   about
+   source/01_getting-started
+   source/02_harmony
+   source/03_loops
+   source/04_minimalism
+   source/05_walks
+   source/06_fractals
+   source/07_genetic
+   source/08_ai
+   source/api
+   source/about
